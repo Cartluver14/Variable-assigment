c810fafb8c967e778e1c0a97c6da21cc8a1fa4a1
North Harrington <339713067@GoTVDSB.ca>
2024-09-10 09:37:43 -0400

    Add project files.

---
+++ Variable assigment/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variable_assigment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String thisroom;
+            int myclass 
+        }
+    }
+}
