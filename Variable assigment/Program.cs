ac23ff26b1263272c770ee38e4d272637117df17
North Harrington <339713067@GoTVDSB.ca>
2024-09-12 09:18:13 -0400

    done

--- Variable assigment/Program.cs
+++ Variable assigment/Program.cs
@@ -10,8 +10,33 @@ namespace Variable_assigment
     {
         static void Main(string[] args)
         {
-            String thisroom;
-            int myclass 
+            //North
+            int number = 29;
+            double price = 2.99, area;
+            string yeah = "I am learing a bit about computer science!";
+            string name;
+            int grad;
+            int radius;
+            Console.WriteLine();
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("This is room #" + number);
+            Console.WriteLine("the price is " + price.ToString("c"));
+            Console.WriteLine(yeah);
+            Console.WriteLine();
+            Console.WriteLine("press Enter to Continue");
+            Console.ReadLine();
+            name = "North Harrington";
+            grad = 2026;
+            Console.WriteLine("my name is " + name + " and ill graduate in " + grad);
+            radius = 8;
+            area = Math.PI * Math.Pow(radius, 2);
+            Console.WriteLine(Math.Round(area, 1));
+           
+
+            
+            
+             
+                
         }
     }
 }
